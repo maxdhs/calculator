7c70e1e5b06270f8f535f9280fd41a0609a15559
Fausto Guillen and Stuart Gill <stuart@>
2018-10-17 13:29:14 -0700

    Add subtract, multiply and divide functions, and a front end interface in JS

--- Calculator/js/scripts.js
+++ Calculator/js/scripts.js
@@ -1,3 +1,5 @@
+// Business (or back-end) logic:
+
 var add = function(number1, number2) {
   return number1 + number2;
 };
@@ -6,7 +8,7 @@ var subtract = function(number1, number2) {
   return number1 - number2;
 };
 
-var multiple = function(number1, number2) {
+var multiply = function(number1, number2) {
   return number1 * number2;
 };
 
@@ -14,8 +16,40 @@ var divide = function(number1, number2) {
   return number1 / number2;
 };
 
-var number1 = parseInt(prompt("Enter a number:"));
-var number2 = parseInt(prompt("Enter another number:"));
+// Everything below this line is user interface (or front-end) logic:
 
-alert(add(number1, number2));
-alert(result);
+$(document).ready(function() {
+  $("form#add").submit(function(event) {
+   event.preventDefault();
+    var number1 = parseInt($("#add1").val());
+    var number2 = parseInt($("#add2").val());
+    var result = add(number1, number2);
+    $("#output").text(result);
+    
+  });
+
+  $("form#subtract").submit(function(event) {
+    event.preventDefault();
+    var number1 = parseInt($("#subtract1").val());
+    var number2 = parseInt($("#subtract2").val());
+    var result = subtract(number1, number2);
+    $("#output").text(result);
+  });
+
+  $("form#divide").submit(function(event) {
+    event.preventDefault();
+    var number1 = parseInt($("#divide1").val());
+    var number2 = parseInt($("#divide2").val());
+    var result = divide(number1, number2);
+    $("#output").text(result);
+  });
+
+  $("form#multiply").submit(function(event) {
+    event.preventDefault();
+    var number1 = parseInt($("#multiply1").val());
+    var number2 = parseInt($("#multiply2").val());
+    var result = multiply(number1, number2);
+    $("#output").text(result);
+  });
+});
+
